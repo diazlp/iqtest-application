3acc274e7adb32aa22e59800e50cb9fc3a7df952
Diaz Linggaputra <diazlinggaputra@gmail.com>
2024-03-17 19:15:26 +0700

    feat: added test question & answer choice

--- src/components/question-card/index.tsx
+++ src/components/question-card/index.tsx
@@ -3,20 +3,13 @@ import Image from 'next/image'
 import { Box, Button, Flex, Grid, GridItem } from '@chakra-ui/react'
 
 export default function QuestionCard(): React.ReactNode {
-  const answerChoice: string[] = [
-    'answer-a',
-    'answer-b',
-    'answer-c',
-    'answer-d',
-    'answer-e',
-    'answer-f',
-  ]
+  const answerChoice: string[] = ['1', '2', '3', '4', '5', '6']
 
   return (
     <Flex mt={10} align="center" direction={{ base: 'column', lg: 'row' }}>
       <Box>
         <Image
-          src={'/assets/question-1/question-1.png'}
+          src={'/assets/question-1/1_0_0.png'}
           alt="Question 1"
           width={450}
           height={300}
@@ -32,7 +25,7 @@ export default function QuestionCard(): React.ReactNode {
           <GridItem key={index} w="100%">
             <Button h="100%" variant="outline" outline="1px">
               <Image
-                src={`/assets/question-1/${choice}.png`}
+                src={`/assets/question-1/1_${choice}_0.png`}
                 alt={choice}
                 width={130}
                 height={130}
